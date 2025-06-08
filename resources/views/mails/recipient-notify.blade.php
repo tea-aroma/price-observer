ba0b0089739512123c9ac4502ff8151976659e38
tea-aroma <kovalyovictor71@gmail.com>
2025-06-08 17:44:58 +0500

    refactor: update the 'recipient-notify' blade template.

--- resources/views/mails/recipient-notify.blade.php
+++ resources/views/mails/recipient-notify.blade.php
@@ -1,3 +1,11 @@
 <h1>Hello, {{ $recipient->name }}!</h1>
 
-<p>Price for this <a href="{{ $itemInformation->url }}">Product</a> was change.</p>
+<p>The price for the product "{{ $itemInformation->name }}" has changed.</p>
+
+<p>Old price: {{ $itemHistory->old_price }} ({{ $itemInformation->currency }})</p>
+
+<p>New price: {{ $itemHistory->new_price }} ({{ $itemInformation->currency }})</p>
+
+<p>You can check the product here: <a href="{{ $itemInformation->url }}">Item</a></p>
+
+<p>Thank you for using our service!</p>
