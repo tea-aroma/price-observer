d99709b240f4f21d86d7d7df47d2a1af147381c9
tea-aroma <kovalyovictor71@gmail.com>
2025-06-09 10:28:26 +0500

    refactor: add check for null.

--- routes/console.php
+++ routes/console.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
 
 
-if (Schema::hasTable('settings'))
+if (Schema::hasTable('settings') && !is_null(\App\Standards\Enums\ItemDelay::NORMAL->data()))
 {
     \Illuminate\Support\Facades\Schedule::command('observer:check-items')
         ->cron(\App\Standards\Enums\ItemDelay::NORMAL->cronSignature())
